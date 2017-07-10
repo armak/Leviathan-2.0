bfd3f91e3f5cf8e42105eaa1c3f8f826b2a28ff4
noby <roope.makinen@student.tut.fi>
2017-07-10 21:44:09 +0300

    initial semi-public alpha-test-version with some features

---
+++ src/main.cpp
@@ -0,0 +1,142 @@
+#define WIN32_LEAN_AND_MEAN
+#define WIN32_EXTRA_LEAN
+#define VC_LEANMEAN
+#define VC_EXTRALEAN
+
+// custom build and feature flags
+#ifdef DEBUG
+	#define OPENGL_DEBUG 1
+	#define FULLSCREEN   0
+	#define CLEAN_EXIT   0
+	#define DESPERATE    0
+#else
+	#define OPENGL_DEBUG 1
+	#define FULLSCREEN   0
+	#define CLEAN_EXIT   0
+	#define DESPERATE    0
+#endif
+
+#define TWO_PASS     1
+#define USE_MIPMAPS  1
+
+#include "definitions.h"
+#if OPENGL_DEBUG
+	#include "debug.h"
+#endif
+
+#include "glext.h"
+#include "shaders/fragment.inl"
+#if TWO_PASS
+	#include "shaders/post.inl"
+#endif
+
+#ifndef EDITOR_CONTROLS
+void entrypoint(void)
+#else
+#include "song.h"
+int CALLBACK WinMain(HINSTANCE prev, HINSTANCE self, LPSTR cmd, int show)
+#endif
+{
+	// initialize window
+	#if FULLSCREEN
+		ChangeDisplaySettings(&screenSettings, CDS_FULLSCREEN);
+		ShowCursor(0);
+		const HDC hDC = GetDC(CreateWindow((LPCSTR)0xC018, 0, WS_POPUP | WS_VISIBLE, 0, 0, XRES, YRES, 0, 0, 0, 0));
+	#else
+		HDC hDC = GetDC(CreateWindow("static", 0, WS_POPUP | WS_VISIBLE, 0, 0, XRES, YRES, 0, 0, 0, 0));
+	#endif	
+
+	// initalize opengl
+	SetPixelFormat(hDC, ChoosePixelFormat(hDC, &pfd), &pfd);
+	wglMakeCurrent(hDC, wglCreateContext(hDC));
+	const int pid = ((PFNGLCREATESHADERPROGRAMVPROC)wglGetProcAddress("glCreateShaderProgramv"))(GL_FRAGMENT_SHADER, 1, &fragment);
+	#if TWO_PASS
+		const int pi2 = ((PFNGLCREATESHADERPROGRAMVPROC)wglGetProcAddress("glCreateShaderProgramv"))(GL_FRAGMENT_SHADER, 1, &post);
+		// not really needed, as long as tid is some value > 0
+		//unsigned int tid;
+		//glGenTextures(1, &tid);
+	#endif
+
+	#if OPENGL_DEBUG
+		shaderDebug(fragment);
+		#if TWO_PASS
+			shaderDebug(post);
+		#endif
+	#endif
+
+	// initialize sound
+	#ifndef EDITOR_CONTROLS
+		CreateThread(0, 0, (LPTHREAD_START_ROUTINE)_4klang_render, lpSoundBuffer, 0, 0);
+		waveOutOpen(&hWaveOut, WAVE_MAPPER, &WaveFMT, NULL, 0, CALLBACK_NULL);
+		waveOutPrepareHeader(hWaveOut, &WaveHDR, sizeof(WaveHDR));
+		waveOutWrite(hWaveOut, &WaveHDR, sizeof(WaveHDR));
+	#else
+		double position = 0.0;
+		song track(L"audio.wav");
+		track.play();
+	#endif
+
+	// main loop
+	do
+	{
+		#if !(DESPERATE)
+			// do minimal message handling so windows doesn't kill your application
+			// not always strictly necessary but increases compatibility a lot
+			MSG msg;
+			PeekMessage(&msg, 0, 0, 0, PM_REMOVE);
+		#endif
+
+		// render with the primary shader
+		((PFNGLUSEPROGRAMPROC)wglGetProcAddress("glUseProgram"))(pid);
+		#ifndef EDITOR_CONTROLS
+			waveOutGetPosition(hWaveOut, &MMTime, sizeof(MMTIME));
+			// remember to divide your shader time variable with the SAMPLE_RATE (44100 with 4klang)
+			((PFNGLUNIFORM1IPROC)wglGetProcAddress("glUniform1i"))(0, MMTime.u.sample);
+		#else
+			position = track.getTime();
+			((PFNGLUNIFORM1IPROC)wglGetProcAddress("glUniform1i"))(0, ((int)(position*44100.0)));
+		#endif
+		glRects(-1, -1, 1, 1);
+
+		// render "post process" using the opengl backbuffer
+		#if TWO_PASS
+			glBindTexture(GL_TEXTURE_2D, 1);
+			#if USE_MIPMAPS
+				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+				glCopyTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, 0, 0, XRES, YRES, 0);
+				((PFNGLGENERATEMIPMAPPROC)wglGetProcAddress("glGenerateMipmap"))(GL_TEXTURE_2D);
+			#else
+				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+				glCopyTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, 0, 0, XRES, YRES, 0);
+			#endif
+			((PFNGLACTIVETEXTUREPROC)wglGetProcAddress("glActiveTexture"))(GL_TEXTURE0);
+			((PFNGLUSEPROGRAMPROC)wglGetProcAddress("glUseProgram"))(pi2);
+			((PFNGLUNIFORM1IPROC)wglGetProcAddress("glUniform1i"))(0, 0);
+			glRects(-1, -1, 1, 1);
+		#endif
+
+		SwapBuffers(hDC);
+
+		// pausing and seeking enabled in debug mode
+		#ifdef EDITOR_CONTROLS
+			double seek = 0.0;
+			if(GetAsyncKeyState(VK_DOWN)) track.pause();
+			if(GetAsyncKeyState(VK_UP))   track.play();
+			if(GetAsyncKeyState(VK_RIGHT) && !GetAsyncKeyState(VK_SHIFT)) seek += 1.0;
+			if(GetAsyncKeyState(VK_LEFT)  && !GetAsyncKeyState(VK_SHIFT)) seek -= 1.0;
+			if(GetAsyncKeyState(VK_RIGHT) && GetAsyncKeyState(VK_SHIFT))  seek += 0.1;
+			if(GetAsyncKeyState(VK_LEFT)  && GetAsyncKeyState(VK_SHIFT))  seek -= 0.1;
+			if(position+seek != position)
+			{
+				position += seek;
+				track.seek(position);
+			}
+		#endif
+	} while(!GetAsyncKeyState(VK_ESCAPE) && MMTime.u.sample < MAX_SAMPLES);
+
+	#if CLEAN_EXIT
+		ChangeDisplaySettings(0, 0);
+		ShowCursor(1);
+	#endif
+	ExitProcess(0);
+}
---
+++ src/song.cpp
@@ -0,0 +1,97 @@
+#include <string>
+#include "song.h"
+#pragma comment(lib, "strmiids.lib")
+#pragma comment(lib, "ComCtl32.lib")
+#pragma comment(lib, "winmm.lib")
+
+#pragma comment(lib, "strmbase.lib")
+
+song::song(LPCWSTR path) : playing(false) {
+	__int64 trackLength;
+	IGraphBuilder * graph;
+
+	CoInitialize(0);
+	CoCreateInstance(CLSID_FilterGraph, 0, CLSCTX_INPROC, IID_IGraphBuilder, (void**)&graph);
+
+	graph->QueryInterface(IID_IMediaControl, (void**)&mediaControl);
+	graph->QueryInterface(IID_IMediaSeeking, (void**)&mediaSeeking);
+	graph->QueryInterface(IID_IBasicAudio, (void**)&audioControl);
+	audioControl->put_Volume(long(-90000));
+
+	HRESULT hr = graph->RenderFile(path, 0);
+
+	if (hr == S_OK)
+		printf("Song succesfully rendered!\n");
+	else
+		printf("mp3 rendering failed :(\n");
+
+	graph->Release();
+
+	mediaSeeking->SetTimeFormat(&TIME_FORMAT_MEDIA_TIME);
+	mediaSeeking->GetDuration(&trackLength);
+	length = (long double)(trackLength) / (long double)10000000.0;
+
+	__int64 position = 0;
+	mediaSeeking->SetPositions(&position, AM_SEEKING_AbsolutePositioning, &position, AM_SEEKING_NoPositioning);
+	pause();
+}
+
+song::~song() {
+	audioControl->Release();
+	mediaControl->Release();
+	mediaSeeking->Release();
+}
+
+int song::play() {
+	mediaControl->Run();
+	playing = true;
+	return 0;
+}
+
+int song::pause() {
+	mediaControl->Stop();
+	playing = false;
+	return 0;
+}
+
+int song::toggle() {
+	playing = !playing;
+	if (playing)
+		play();
+	else
+		pause();
+	return 0;
+}
+
+bool song::is_playing() {
+	OAFilterState state;
+	mediaControl->GetState(INFINITE, &state);
+	__int64 trackLength, position;
+	mediaSeeking->GetDuration(&trackLength);
+	mediaSeeking->GetCurrentPosition(&position);
+	return position<trackLength;
+}
+
+int song::seek(long double position) {
+
+	if (position>length)
+		position = length;
+	if (position<.0)
+		position = .0;
+
+	__int64 seekPosition = __int64(10000000.0*position);
+
+	if (playing)
+		mediaControl->Stop();
+	mediaSeeking->SetPositions(&seekPosition, AM_SEEKING_AbsolutePositioning, &seekPosition, AM_SEEKING_NoPositioning);
+	if (playing)
+		mediaControl->Run();
+
+	return 0;
+}
+
+long double song::getTime() {
+	__int64 position;
+	mediaSeeking->GetCurrentPosition(&position);
+	return (long double)(position) / (long double)10000000.0;
+}
