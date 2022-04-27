9d89e4dc271de78d7a93735c23c0e748113dab79
Arttu Tamminen <arttu.tamminen@kapsi.fi>
2022-04-27 20:22:22 +0300

    example 4klang 3.11 song

--- src/4klang/4klang.h
+++ src/4klang/4klang.h
@@ -1,19 +1,22 @@
 // some useful song defines for 4klang
 #define SAMPLE_RATE 44100
-#define BPM 120.000000
-#define MAX_INSTRUMENTS 5
-#define MAX_PATTERNS 136
-#define PATTERN_SIZE_SHIFT 3
+#define BPM 110.000000
+#define MAX_INSTRUMENTS 3
+#define MAX_PATTERNS 9
+#define PATTERN_SIZE_SHIFT 4
 #define PATTERN_SIZE (1 << PATTERN_SIZE_SHIFT)
 #define MAX_TICKS (MAX_PATTERNS*PATTERN_SIZE)
-#define SAMPLES_PER_TICK 5512
+#define SAMPLES_PER_TICK 6013
 #define MAX_SAMPLES (SAMPLES_PER_TICK*MAX_TICKS)
-#define POLYPHONY 2
-#define INTEGER_16BIT
-#define SAMPLE_TYPE short
+#define POLYPHONY 1
+#define FLOAT_32BIT
+#define SAMPLE_TYPE float
 
 #define WINDOWS_OBJECT
 
 // declaration of the external synth render function, you'll always need that
-extern "C" void __stdcall _4klang_render(void*);
+extern "C" void  __stdcall	_4klang_render(void*);
 // declaration of the external envelope buffer. access only if you're song was exported with that option
+extern "C" float _4klang_envelope_buffer;
+// declaration of the external note buffer. access only if you're song was exported with that option
+extern "C" int   _4klang_note_buffer;
